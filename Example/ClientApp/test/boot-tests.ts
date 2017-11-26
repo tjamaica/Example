a454331974f67c6c558f9e0689f655192f5aa6c7
t.elmadady <t.elmadady@gmail.com>
2017-11-26 23:24:59 +0200

    example

--- Example/ClientApp/test/boot-tests.ts
+++ Example/ClientApp/test/boot-tests.ts
@@ -11,11 +11,11 @@ import * as testing from '@angular/core/testing';
 import * as testingBrowser from '@angular/platform-browser-dynamic/testing';
 
 // There's no typing for the `__karma__` variable. Just declare it as any
-declare var __karma__: any;
+declare var karma: any;
 declare var require: any;
 
 // Prevent Karma from running prematurely
-__karma__.loaded = function () {};
+karma.loaded = function () {};
 
 // First, initialize the Angular testing environment
 testing.getTestBed().initTestEnvironment(
@@ -29,5 +29,5 @@ const context = require.context('../', true, /\.spec\.ts$/);
 // And load the modules
 context.keys().map(context);
 
-// Finally, start Karma to run the tests
-__karma__.start();
+// Finally, start Karma to run the test
+karma.start();
